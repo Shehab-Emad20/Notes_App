9d4aea1da8c951c06586524d1a489a010f3ce4e9
Shehab-Emad20 <shehabz2014@gmail.com>
2024-08-26 19:47:35 -0700

    foramte date time and fix the error

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_button.dart';
@@ -54,10 +55,13 @@ class _AddNewNoteFormState extends State<AddNewNoteForm> {
                 onTap: () {
                   if (formKey.currentState!.validate()) {
                     formKey.currentState!.save();
+                    var currentDate = DateTime.now();
+                    var formatedcurrentDate =
+                        DateFormat.yMd().format(currentDate);
                     var notModel = NoteModel(
                         title: title!,
                         subTitle: subTile!,
-                        data: DateTime.now().toString(),
+                        data: formatedcurrentDate,
                         color: Colors.blue.value);
                     BlocProvider.of<AddNoteCubit>(context).addNote(notModel);
                   } else {
